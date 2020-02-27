4df21969da2eb379b6c386e03fff5cc9edca9711
cramos78 <60783950+cramos78@users.noreply.github.com>
2020-02-26 19:49:47 -0500

    Add files via upload
    Adding initial skeleton files to provide an overall high level structure of code. This structure may change as code is developed and a better way to reorganize is needed.

---
+++ Project2/Dijkstra_rigid.py
@@ -0,0 +1,29 @@
+import pygame as pyg
+
+
+# Given a predefined map, convert to binary map to determine obstacle space (point robot radius = r, obstacle clearance = c)
+
+
+
+# Obtaining Input from user : Start Point and Goal Point
+
+
+
+# Check feasibility of  User Input : For start or goal nodes in obstacle space or outside range of binary map, inform user to redefine points try again
+
+
+
+# Implement Djikstra's Algorithm
+
+
+
+# Implementing Backtracking (once goal node is achieved) 
+
+
+
+# Using pygame implement animation of optimal path generation
+
+
+
+
+
