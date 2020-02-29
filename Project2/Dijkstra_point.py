c3af2e50fbf45aa4d06809b9a441223fdd29c6ef
sully892 <sully892.ts@gmail.com>
2020-02-28 19:15:27 -0500

    cleaned up code and integrated (for now) changes

--- Project2/Dijkstra_point.py
+++ Project2/Dijkstra_point.py
@@ -1,9 +1,9 @@
 import pygame as pyg
-<<<<<<< HEAD
 import sys
 import numpy as np
 from pygame.locals import *
 import math as m
+import heapq as hpq
 
 
 def GetLine(point1, point2):
@@ -20,16 +20,10 @@ def GetLine(point1, point2):
         if (point2[1] < point1[1]):
             y = point1[1] - 1
         line.append((x, y))
+    return line
 
 
-return line
-=======
-import heapq as hpq
-
->>>>>>> 429eb4a7295b5623a5d6d422c023be1352f6cf29
 # Given a predefined map, convert to binary map to determine obstacle space (point robot radius = 0, obstacle clearance = 0)
-
-
 def CreateMap():
     # Create map dictionary which holds coordinates as a tuple of ints and the pixel value of that coordinate location as an int
     map_keys = []
@@ -41,17 +35,17 @@ def CreateMap():
     map_space = dict(zip(map_keys, map_values))
 
     obstacles = []
-    poly = []
-    poly_coords = [(25, 185), (75, 185), (100, 150), (75, 120), (50, 150),
-                   (20, 120)]
-    for n in range(0, len(poly_coords), 2):
-        poly.append(FindLine(poly_coords[n], poly_coords[n + 1]))
-    obstacles.append(poly)
+    poly1 = []
+    poly1_coords = [(25, 185), (75, 185), (100, 150), (75, 120), (50, 150),
+                    (20, 120)]
+    for n in range(0, len(poly1_coords), 2):
+        poly1.append(GetLine(poly1_coords[n], poly1_coords[n + 1]))
+    obstacles.append(poly1)
 
     rect = []
     rect_coords = [(30, 131), (39, 136), (104, 164), (95, 169)]
     for n in range(0, len(rect_coords), 2):
-        rect.append(FindLine(rect_coords[n], rect_coords[n + 1]))
+        rect.append(GetLine(rect_coords[n], rect_coords[n + 1]))
     obstacles.append(rect)
 
     oval = []
@@ -67,7 +61,15 @@ def CreateMap():
     circle_start = (149, 99)
     r = 25
     for x in range(0, 25 * 2):
-        circle.append(x, 50 + m.sqrt(r**2 - (x - 224)**2))
+        circle.append(x, 50 + abs(m.sqrt(r**2 - (x - 224)**2)))
+        circle.append(x, 50 + -abs(m.sqrt(r**2 - (x - 224)**2)))
+    obstacles.append(circle)
+
+    poly2 = []
+    poly2_coords = []
+    for n in range(0, len(poly2_coords), 2):
+        poly1.append(GetLine(poly2_coords[n], poly2_coords[n + 1]))
+    obstacles.append(poly2)
 
     for obstacle in obstacles:
         for point in obstacle:
@@ -101,12 +103,8 @@ def GetInput(map_space):
                     goal_string = element
                 else:
                     goal_string = goal_string + ', ' + element
-        print(start_string)
-        print(goal_string)
         start = tuple(map(int, start_string.split(', ')))
         goal = tuple(map(int, goal_string.split(', ')))
-        print(start)
-        print(goal)
         if (0 < (start[0] and goal[0]) < 300):
             if (0 < (start[1] and goal[1]) < 200):
                 if (map_space[start] == 1):
@@ -114,98 +112,97 @@ def GetInput(map_space):
                         input_bad = False
     return start, goal
 
-<<<<<<< HEAD
 
 # Implement Djikstra's Algorithm
 # Implementing Backtracking (once goal node is achieved)
 def Solve(start, goal):
     pass
 
-=======
-#Implementing a data structure (priority queue) to store Nodes
-ListOfNeighborsMoves =       [(0,1),(1,0),(0,-1),(-1,0),(-1,1),(1,1),(1,-1),(-1,-1)]
-ListOfNeighborsMovesCost = [   1  ,   1  ,    1   ,    1  ,  1.4  , 1.4 ,  1.4  ,  1.4   ]
 
 class PointNode:
-	def __init__(self, Node_State_i = [], Node_Cost_i = 0, Node_Parent_i = 0):
-		self.Node_State_i = Node_State_i
-		self.Node_Cost_i = Node_Cost_i
-		self.Node_Parent_i = Node_Parent_i
+    def __init__(self, Node_State_i=[], Node_Cost_i=0, Node_Parent_i=0):
+        self.Node_State_i = Node_State_i
+        self.Node_Cost_i = Node_Cost_i
+        self.Node_Parent_i = Node_Parent_i
 
-#Implementing functions to add and get Nodes from the PointNode priority queue 
-def addNewNode(point_Node,newNode_cost,newNode):
-	hpq.heappush(point_Node.Node_State_i,(newNode_cost,newNode))
-	
+
+#Implementing functions to add and get Nodes from the PointNode priority queue
+def addNewNode(point_Node, newNode_cost, newNode):
+    hpq.heappush(point_Node.Node_State_i, (newNode_cost, newNode))
+
+
 def getNode(currentNode):
-	node_removed = hpq.heappop(currentNode.Node_State_i)
-	node = node_removed[1]
-	cost = node_removed[0]
-	return node, cost
-	
+    node_removed = hpq.heappop(currentNode.Node_State_i)
+    node = node_removed[1]
+    cost = node_removed[0]
+    return node, cost
+
+
 def getCost(current_node_cost, new_node_move):
-	index = ListOfNeighborsMoves.index(new_node_move)
-	new_node_cost =  current_node_cost + ListOfNeighborsMovesCost[index]
-	return new_node_cost
+    index = ListOfNeighborsMoves.index(new_node_move)
+    new_node_cost = current_node_cost + ListOfNeighborsMovesCost[index]
+    return new_node_cost
+
+
 # ############################################
 #Testing adding and removing Nodes from PointNode:
 
-def priorityQueueTest():
-	startNode = [1,2]
-	ListOfNodes = PointNode()
-	print("Empty list = ",ListOfNodes.Node_State_i)
-	addNewNode(ListOfNodes,13,startNode)
-	print("List after adding startNode = ",ListOfNodes.Node_State_i)
-	newNode1 = [13,4]
-	addNewNode(ListOfNodes,12,newNode1)
-	print("List after adding newNode1 = ",ListOfNodes.Node_State_i)
-	newNode2 = [5,6]
-	addNewNode(ListOfNodes,16,newNode2)
-	print("List after adding newNode2 = ",ListOfNodes.Node_State_i)
-	newNode3 = [12,3]
-	addNewNode(ListOfNodes,5,newNode3)
-	print("List after adding newNode3 = ",ListOfNodes.Node_State_i)
 
-	getNode(ListOfNodes)
-	print(ListOfNodes.Node_State_i)
-	getNode(ListOfNodes)
-	print(ListOfNodes.Node_State_i)
+def priorityQueueTest():
+    startNode = [1, 2]
+    ListOfNodes = PointNode()
+    print("Empty list = ", ListOfNodes.Node_State_i)
+    addNewNode(ListOfNodes, 13, startNode)
+    print("List after adding startNode = ", ListOfNodes.Node_State_i)
+    newNode1 = [13, 4]
+    addNewNode(ListOfNodes, 12, newNode1)
+    print("List after adding newNode1 = ", ListOfNodes.Node_State_i)
+    newNode2 = [5, 6]
+    addNewNode(ListOfNodes, 16, newNode2)
+    print("List after adding newNode2 = ", ListOfNodes.Node_State_i)
+    newNode3 = [12, 3]
+    addNewNode(ListOfNodes, 5, newNode3)
+    print("List after adding newNode3 = ", ListOfNodes.Node_State_i)
+
+    getNode(ListOfNodes)
+    print(ListOfNodes.Node_State_i)
+    getNode(ListOfNodes)
+    print(ListOfNodes.Node_State_i)
+
 
 # ############################################
-start_Node = (4,5)
-goal_Node = (20,20)
+
 
 # Implementing Djikstra's Algorithm
 def applyingDijkstraAlgorithm(start_node, goal_node):
-	exploredNodesPath = {}
-	exploredNodesCost = {}
-	exploredNodesPath[start_Node] = 0
-	exploredNodesCost[start_Node] = 0
-	ListOfNodes = PointNode()
-	addNewNode(ListOfNodes,0,start_Node)
-	print("Explored Node Cost ",exploredNodesCost)
-	print("List of Nodes ",ListOfNodes.Node_State_i)
-	while len(ListOfNodes.Node_State_i) > 0:
-		currentNode, currentNodeCost= getNode(ListOfNodes)
-		print("Current Node ",currentNode)
-		print("Current Node Cost = ", currentNodeCost)
-		if currentNode == goal_node:
-			break
-		for newNodeMove in ListOfNeighborsMoves:
-			newNode = (currentNode[0] + newNodeMove[0],currentNode[1] + newNodeMove[1])
-			print("New Node = ",newNode)
-			newNodeCost = currentNodeCost + getCost(currentNodeCost,newNodeMove)
-			print("New Cost =" , newNodeCost)
-			if newNode not in exploredNodesCost or newNodeCost < exploredNodesCost[newNode]:
-				exploredNodesCost[newNode] = newNodeCost
-				addNewNode(ListOfNodes,newNodeCost,newNode)
-				exploredNodesPath[newNode] = newNodeCost
-	return exploredNodesPath	
-		
-		
-	
-applyingDijkstraAlgorithm(start_Node,goal_Node)
-	
->>>>>>> 429eb4a7295b5623a5d6d422c023be1352f6cf29
+    exploredNodesPath = {}
+    exploredNodesCost = {}
+    exploredNodesPath[start_Node] = 0
+    exploredNodesCost[start_Node] = 0
+    ListOfNodes = PointNode()
+    addNewNode(ListOfNodes, 0, start_Node)
+    print("Explored Node Cost ", exploredNodesCost)
+    print("List of Nodes ", ListOfNodes.Node_State_i)
+    while len(ListOfNodes.Node_State_i) > 0:
+        currentNode, currentNodeCost = getNode(ListOfNodes)
+        print("Current Node ", currentNode)
+        print("Current Node Cost = ", currentNodeCost)
+        if currentNode == goal_node:
+            break
+        for newNodeMove in ListOfNeighborsMoves:
+            newNode = (currentNode[0] + newNodeMove[0],
+                       currentNode[1] + newNodeMove[1])
+            print("New Node = ", newNode)
+            newNodeCost = currentNodeCost + getCost(currentNodeCost,
+                                                    newNodeMove)
+            print("New Cost =", newNodeCost)
+            if newNode not in exploredNodesCost or newNodeCost < exploredNodesCost[
+                    newNode]:
+                exploredNodesCost[newNode] = newNodeCost
+                addNewNode(ListOfNodes, newNodeCost, newNode)
+                exploredNodesPath[newNode] = newNodeCost
+    return exploredNodesPath
+
 
 # Using pygame implement animation of map exploration and display optimal path
 
@@ -232,11 +229,16 @@ def Visualize(path, map_space):
 
 #main function which calls all subfunctions necessary to solve the maze with Dijkstra's algorithm
 def main():
+    #Implementing a data structure (priority queue) to store Nodes
+    ListOfNeighborsMoves = [(0, 1), (1, 0), (0, -1), (-1, 0), (-1, 1), (1, 1),
+                            (1, -1), (-1, -1)]
+    ListOfNeighborsMovesCost = [1, 1, 1, 1, 1.4, 1.4, 1.4, 1.4]
+    #start_Node = (4,5)
+    #goal_Node = (20,20)
     map_space = CreateMap()
-    print(type(map_space))
     start, goal = GetInput(map_space)
     #path = Solve(start, goal)
-    path = 1
+    #path = applyingDijkstraAlgorithm(start_Node,goal_Node)
     Visualize(path, map_space)
 
 
